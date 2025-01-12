365b6e282430be85d5fbdf894040bfdabf619d89
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-01-12 17:16:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
     load_dotenv()
 
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
-    MONGODB_URL = os.getenv("MONGODB_URL")
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL"))
+    API_ID = int(os.getenv("API_ID", ""))
+    API_HASH = os.getenv("API_HASH", "")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+    MONGODB_URL = os.getenv("MONGODB_URL", "")
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
     REACTION_EMOJI = os.getenv("REACTION_EMOJI", "👍")
     ADMIN_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_IDS", "").split(",") if admin_id]
