2a9a04d648f2e91360b534748bef3154dc8aee47
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-01-12 09:38:59 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_ID = int(os.getenv('API_ID'))
+API_HASH = os.getenv('API_HASH')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+DATABASE_URL = os.getenv('DATABASE_URL', 'sport_bot.db')
+LOG_CHANNEL_ID = os.getenv('LOG_CHANNEL_ID')
