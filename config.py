0ce4eb8c4188d7fa1bb9890b3ae3cc2354604c7c
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-01-12 17:34:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,4 +9,4 @@
     MONGODB_URL = os.getenv("MONGODB_URL", "")
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
     REACTION_EMOJI = os.getenv("REACTION_EMOJI", "👍")
-    ADMIN_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_IDS", "").split(",") if admin_id]
+    ADMIN_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_IDS", "7841292070").split(",") if admin_id]
