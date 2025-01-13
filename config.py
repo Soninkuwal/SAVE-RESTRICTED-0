d460ea13486d13a4c31ab42605cc9f9360dc6eb4
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-01-13 10:10:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,17 @@
 
     load_dotenv()
 
-    API_ID = int(os.getenv("API_ID", ""))
-    API_HASH = os.getenv("API_HASH", "")
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-    MONGODB_URL = os.getenv("MONGODB_URL", "")
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", ""))
+    API_ID = int(os.getenv("API_ID"))
+    API_HASH = os.getenv("API_HASH")
+    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    MONGODB_URL = os.getenv("MONGODB_URL")
     REACTION_EMOJI = os.getenv("REACTION_EMOJI", "👍")
-    ADMIN_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_IDS", "7841292070").split(",") if admin_id]
+
+    # Load Admin Ids from .env or empty list
+    ADMIN_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_IDS", "").split(",") if admin_id] or []
+    # Load Log channel id from .env if it exist
+    LOG_CHANNEL = os.getenv("LOG_CHANNEL")
+    if LOG_CHANNEL:
+        LOG_CHANNEL = int(LOG_CHANNEL) # convert to integer
+    else:
+       LOG_CHANNEL = None #Set None if the LOG_CHANNEL variable is missing
