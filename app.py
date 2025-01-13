85c5c44b351101715e901f03c1cbc56ec1cab341
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2025-01-13 11:05:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
+    from bot.main import main
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Tech VJ'
-
-
-if __name__ == "__main__":
-    app.run()
+    if __name__ == '__main__':
+        main()
